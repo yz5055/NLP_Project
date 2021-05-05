f138551e7eab39a2efca35ab0d02f8ff99d5bd47
Christopher Liu <christopherliu830@gmail.com>
2021-05-05 03:49:00 -0400

    svm and other features

--- classifier.py
+++ classifier.py
@@ -2,8 +2,11 @@ from covid_q import train, test
 from features import build_features, server as dependency_server
 from nltk.classify.maxent import train_maxent_classifier_with_iis
 from nltk.parse import corenlp
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.svm import LinearSVC
 import atexit
 
+scikit = SklearnClassifier(LinearSVC())
 dependency_server.start()
 atexit.register(dependency_server.stop)
 
@@ -14,25 +17,33 @@ with open('log', 'w') as log:
 
 
         # Train classifier
+        scikit.train(training_data)
         ment = train_maxent_classifier_with_iis(training_data)
 
         correct = 0
+        correct_svm = 0
         incorrect = 0
-
-        i = 0
+        incorrect_svm = 0
 
         # Build features for test data
-        for features, label in test.apply(build_features, axis=1):
+        for idx, (features, label) in enumerate(test.apply(build_features, axis=1)):
+            found_label_svm ,= scikit.classify_many([features])
             found_label = ment.classify(features)
-            log.write(f'{test.loc[i, "Question"]}\n')
+
+            log.write(f'{test.loc[idx, "Question"]}\n')
+            feat_string = ''.join([f'{key}={val} ' for key,val in features.items()])
             if found_label == label: 
                 correct += 1
                 log.write(f'\t{feat_string} -> {found_label}\n')
             else:
                 incorrect += 1
-                feat_string = ''.join([f'{key}={val} ' for key,val in features.items()])
                 log.write(f'!\t{feat_string} -!> {found_label}, {label}\n')
-            log.write('\n')
-            i += 1
+            
+            if found_label_svm == label:
+                correct_svm += 1
+            else:
+                incorrect_svm += 1
 
-        print(f'Final accuracy: {correct/incorrect}')
+            log.write('\n')
+        print(f'Final accuracy (maxent): {correct/incorrect}')
+        print(f'Final accuracy (svm): {correct_svm/incorrect_svm}')
--- features.py
+++ features.py
@@ -20,9 +20,10 @@ wh_words = [
     "when",
     "where",
     "who",
+    "how long",
+    "are",
     "will",
     "is there",
-    "how long",
     "how", 
     "can",
     "would",
@@ -31,26 +32,87 @@ wh_words = [
 
 # Find the root word of the sentence
 def dep_parse(sentence):
-    parse, = dep_parser.raw_parse(sentence)
+
+    # replace "special" vocabulary i.e covid
+    to_parse = make_substitutions(sentence)
+
+    parse, = dep_parser.raw_parse(to_parse)
     conll = parse.to_conll(4)
     tree = parse.tree()
     
     feats = { }
 
-    for line in conll.split("\n")[:-1]:
+    root_pos = -1
+    covid_pos = -1
+
+    for idx, line in enumerate(conll.split("\n")[:-1], start=1):
         word, tag, head, rel = line.split("\t")
-        if rel == "nsubj": feats["subject"] = word
-        elif rel == "ROOT": feats["head_word"] = word 
+
+        if rel == "ROOT": 
+            root_pos = rel
+            feats["root"] = word 
+
+            if tag == "NN":
+                feats["head_noun"] = word
+        
+        if word == "virus":
+            covid_pos = idx + 1
+    
+    for idx, line in enumerate(conll.split("\n")[:-1], start=1):
+        word, tag, head, rel = line.split("\t")
+
+        # A dependent
+        if head == str(covid_pos):
+            feats["dependent"] = (word, tag)
+
+    # If the root wasn't a noun, find the head noun
+    # In addition, find key adjectives
+    if "head_noun" not in feats:
+
+        for idx, line in enumerate(conll.split("\n")[:-1], start=1):
+            word, tag, head, rel = line.split("\t")
+
+            if rel == "nsubj": 
+                feats["head_noun"] = word
+                break
+            elif head == root_pos and tag == "NN": 
+                feats["head_noun"] = word
+                break
+
+    hypernyms(feats)
 
     return feats
+
+def hypernyms(feats):
+    if "head_noun" in feats:
+        word = feats["head_noun"]
+        synsets = wn.synsets(word, wn.NOUN)
+
+        if synsets:
+            hs = synsets[0].hypernyms()
+            if hs: feats["hypernym"] = hs[0].name()
+            else: feats["hypernym"] = word
+
+    if "dependent" in feats:
+        word, tag = feats["dependent"]
+        feats["dependent"] = word
+        if tag[0] in penn_to_wn:
+            synsets = wn.synsets(word, penn_to_wn[tag[0]])
+        else:
+            synsets = wn.synsets(word)
+
+        if synsets:
+            hs = synsets[0].hypernyms()
+            if hs: feats["d_hypernym"] = hs[0].name()
+            else: feats["d_hypernym"] = word
+
+
 
 def build_features(series):
     question = series["Question"]
     label = series["Category"]
 
-    features = { 
-        "wh_word": None,
-    }
+    features = { }
 
     # wh_word
     for wh_word in wh_words:
@@ -62,3 +124,25 @@ def build_features(series):
 
     return (features, label)
 
+
+vocab_map = {
+    "president trump": "the president",
+    "trump": "the president",
+    "bill gates": "he",
+    "covid virus": "virus",
+    "the covid": "the virus",
+    "covid": "the virus",
+    "the covid vaccine": "the vaccine",
+}
+
+penn_to_wn = {
+    "N": wn.NOUN,
+    "J": wn.ADJ,
+    "V": wn.VERB,
+    "R": wn.ADV,
+}
+
+def make_substitutions(sentence):
+    for key, val in vocab_map.items():
+        sentence = sentence.replace(key, val)
+    return sentence
